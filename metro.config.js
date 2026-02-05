e52521d98eb952efad45eeaa455f3ee15b29b48f
BikashBaishnab <baishnabbikash@gmail.com>
2026-02-05 14:48:00 +0530

    fix(web): fix bundling error on web by mocking react-native-image-viewing and updating metro config

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,12 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Fix for react-native-image-viewing on web
+if (process.env.EXPO_PUBLIC_PLATFORM === 'web' || process.env.NODE_ENV === 'production') {
+    config.resolver.extraNodeModules = {
+        ...config.resolver.extraNodeModules,
+        'react-native-image-viewing': 'react-native-web/dist/exports/View',
+    };
+}
+
 module.exports = withNativeWind(config, { input: "./global.css" });
